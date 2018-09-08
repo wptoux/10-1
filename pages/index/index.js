7b4b65eb71bc551f86cb5e51bc7ed2a37500123f
Zhen Wang <wangzhen_ok@bupt.edu.cn>
2018-09-08 23:12:31 +0800

    http

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,13 +8,49 @@ Page({
     imageSrc: ""
   },
 
-  onStartTap: function(){
+  onGotUserInfo: function (e) {
     wx.navigateTo({
       url: '../record/record',
     })
+
+    console.info("User Info:")
+    console.log(e)
+    // 登录
+    wx.login({
+      success: res => {
+        console.info("WX Login status:")
+        console.info(res)
+
+        let params = e.detail.userInfo
+        params['wxCode'] = res.code
+        
+        wx.request({
+          url: app.globalData.baseUrl + '/user/logintest',
+          data: params,
+          method: 'POST',
+          success: (resLogin) => {
+            console.info("Server Login status:")
+            console.info(resLogin)
+
+            if (resLogin.statusCode == 200 && resLogin.data.code == 1) {
+              app.globalData.cookie = resLogin.header['Set-Cookie']
+              app.globalData.loginStatus = 1
+            }
+            else{
+              console.warn('Server login failed...')
+              app.globalData.loginStatus = -1
+            }
+          },
+          fail: () => {
+            console.warn('Server login failed...')
+            app.globalData.loginStatus = -1
+          }
+        })
+      }
+    })
   },
 
-  onLoad: function(){
+  onLoad: function () {
     this.setData({
       isImageReady: true,
       imageSrc: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1535798551&di=2ef16e231335625e1a51e898fc6cc612&imgtype=jpg&er=1&src=http%3A%2F%2Fwww.wallcoo.com%2Fcartoon%2FKitsunenoir_Design_Illustration_V%2Fwallpapers%2F2560x1440%2Fkim-holtermand-reflections.jpg"
--- pages/record/record.js
+++ pages/record/record.js
@@ -1,6 +1,7 @@
 // pages/record/record.js
 var timer;
 var recordData;
+var app = getApp()
 Page({
 
   /**
@@ -22,16 +23,20 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    this.setData({
-      isHintReady: true,
-      hintImgSrc: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1535798551&di=2ef16e231335625e1a51e898fc6cc612&imgtype=jpg&er=1&src=http%3A%2F%2Fwww.wallcoo.com%2Fcartoon%2FKitsunenoir_Design_Illustration_V%2Fwallpapers%2F2560x1440%2Fkim-holtermand-reflections.jpg"
-    })
-
+  onLoad: function(options) {
     var that = this;
+
+    while(app.globalData.loginStatus === 0){
+      setTimeout(() => {}, 30)
+    }
+
+    if (app.globalData.loginStatus === -1){
+      return
+    }
+
     //获取屏幕的宽度
     wx.getSystemInfo({
-      success: function (res) {
+      success: (res) => {
         that.setData({
           phoneWidth: res.windowWidth,
           phoneHeight: res.windowHeight
@@ -40,25 +45,82 @@ Page({
     })
     this.openBarrage()
 
-    var texts = ["123", "234", "345", "456", "678", "789"]
-    for (var i = 0; i < texts.length; i++) {
-      this.shoot(texts[i])
-    }
+    //获取Hint
+    wx.request({
+      url: app.globalData.baseUrl + '/greeting/random',
+      method: 'GET',
+      header: {
+        'cookie': app.globalData.cookie
+      },
+      success: (res) => {
+        console.info('Get hint status:')
+        console.info(res)
+        if (res.statusCode == 200 && res.data.code == 1){
+          that.setData({
+            isHintReady: true,
+            hintImgSrc: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1535798551&di=2ef16e231335625e1a51e898fc6cc612&imgtype=jpg&er=1&src=http%3A%2F%2Fwww.wallcoo.com%2Fcartoon%2FKitsunenoir_Design_Illustration_V%2Fwallpapers%2F2560x1440%2Fkim-holtermand-reflections.jpg",
+            hintText: res.data.data.content
+          })
+        }
+        else{
+          that.setData({
+            isHintReady: true,
+            hintImgSrc: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1535798551&di=2ef16e231335625e1a51e898fc6cc612&imgtype=jpg&er=1&src=http%3A%2F%2Fwww.wallcoo.com%2Fcartoon%2FKitsunenoir_Design_Illustration_V%2Fwallpapers%2F2560x1440%2Fkim-holtermand-reflections.jpg",
+            hintText: "祖国您好~"
+          })
+        }
+      }
+    })
+
+    //获取弹幕
+    let texts = null;
+
+    wx.request({
+      url: app.globalData.baseUrl + '/voice/list?page=0',
+      method: 'GET',
+      header: {
+        'cookie': app.globalData.cookie
+      },
+      success: function(res) {
+        console.info('Get list status:')
+        console.info(res)
+        if (res.statusCode == 200 && res.data.code == 1 && res.data.data.length > 0) {
+          texts = res.data.data
+        } else {
+          texts = ["123", "234", "345", "456", "678", "789"]
+        }
+
+        for (var i = 0; i < texts.length; i++) {
+          that.shoot(texts[i])
+        }
+      }
+    })
 
     wx.getRecorderManager().onStop((res) => {
-      recordData = res;
+      console.info('Record result:')
+      console.info(res)
+      recordData = res.tempFilePath
       wx.navigateTo({
-        url: '../share/share?record_data=' + recordData,
+        url: '../share/share?record_data=' + encodeURI(recordData),
       })
 
     })
   },
 
-  onRecordPressed: function (e) {
-    wx.getRecorderManager().start()
+  onRecordPressed: function(e) {
+    const options = {
+      // duration: 10000,
+      sampleRate: 16000,
+      numberOfChannels: 1,
+      encodeBitRate: 96000,
+      format: 'mp3',
+      frameSize: 50
+    }
+    wx.getRecorderManager().start(options)
+
     var barrage_style_arr = this.data.barrage_style;
     var that = this;
-    timer = setInterval(function () {
+    timer = setInterval(function() {
       for (var i = 0; i < barrage_style_arr.length; i++) {
         barrage_style_arr[i].left += barrage_style_arr[i].speed;
         if (barrage_style_arr[i].left > that.data.phoneWidth) {
@@ -73,17 +135,17 @@ Page({
 
     }, 100);
   },
-  onRecordReleased: function (e) {
+  onRecordReleased: function(e) {
     clearInterval(timer);
     wx.getRecorderManager().stop()
   },
-  onRecordCancelled: function (e) {
+  onRecordCancelled: function(e) {
     clearInterval(timer);
     wx.getRecorderManager().stop()
   },
 
   //是否打开弹幕... 
-  openBarrage: function () {
+  openBarrage: function() {
     this.setData({
       barrageTextColor: "#04BE02",
       barrage_inputText: "flex",
@@ -91,7 +153,7 @@ Page({
     });
   },
 
-  shoot: function (text) {
+  shoot: function(text) {
     //字体颜色随机
     var textColor = "rgb(" + parseInt(Math.random() * 256) + "," + parseInt(Math.random() * 256) + "," + parseInt(Math.random() * 256) + ")";
     var top = (Math.random()) * this.data.phoneHeight * 0.65;
