3ef909b5c2598f6f72aa577ea40f127684f939cf
Zhen Wang <wangzhen_ok@bupt.edu.cn>
2018-09-26 14:43:22 +0800

    use canvas to improve performance

--- pages/record/record.js
+++ pages/record/record.js
@@ -16,10 +16,6 @@ Page({
     hintImgSrc: "../../assets/hint_back.png",
     hintText: "",
 
-    barrageBackImgSrc: "../../assets/barrage_back.png",
-    barrageStyle: [],
-    barrageflyDisplay: "none",
-
     btnImgSrc: "../../assets/recorder.png",
   },
 
@@ -50,15 +46,38 @@ Page({
         console.info('Record result:')
         console.info(res)
         let recordFilePath = res.tempFilePath
-        wx.navigateTo({
-          url: '../share/share?recordFilePath=' + encodeURIComponent(recordFilePath),
+
+        wx.uploadFile({
+          url: app.globalData.baseUrl + '/voice/recognize',
+          filePath: recordFilePath,
+          name: 'file',
+          header: {
+            'cookie': app.globalData.cookie
+          },
+          success: function (res) {
+            console.info('Recognize result:')
+            console.info(res)
+            if (res.statusCode == 200) {
+              wx.navigateTo({
+                url: '../share/share?result=' + encodeURIComponent(res),
+              })
+            }
+            else{
+              wx.navigateTo({
+                url: '../share/share?result=' + encodeURIComponent('{"code":0}'),
+              })
+            }
+          },
+          fail: () =>{
+            wx.navigateTo({
+              url: '../share/share?result=' + encodeURIComponent('{"code":0}'),
+            })
+          }
         })
       }
     })
 
-    this.setData({
-      barrageStyle: app.globalData.barrageStatus
-    })
+    this.drawBarrage(app.globalData.barrageStatus)
   },
 
   onRecordPressed: function (e) {
@@ -124,5 +143,69 @@ Page({
         }
       }
     })
-  }
+  },
+  drawBarrage: function (barrageStyle) {
+    let c = wx.createCanvasContext('canvas', this)
+    let px = this.data.phoneWidth / 750
+    let rem = this.data.phoneWidth / 20
+
+    function _drawItem(x, y, avatar, text) {
+      /*
+      .barrage-fly-obj {
+        position: absolute;
+        width: 12.5rem;
+        height: 2.5rem;
+        overflow: hidden;
+      }
+
+      .barrage-content {
+        position: absolute;
+        top: 1.25rem;
+        left: 2.1rem;
+        width: 9rem;
+        height: 1.5rem;
+      }
+
+      .barrage-content image {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 1rem;
+        height: 1rem;
+      }
+
+      .barrage-content text {
+        position: absolute;
+        top: 0;
+        left: 1rem;
+        width: 8rem;
+        font-size: 0.8rem;
+        text-align: right;
+      }
+
+      .barrage-back {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: -1;
+      }
+      */
+
+      c.drawImage("../../assets/barrage_back.png", x, y, 12.5 * rem, 2.5 * rem)
+      c.drawImage(avatar, x + 2.1 * rem, y + 1.25 * rem, rem, rem)
+      c.setFontSize(0.8 * rem)
+      c.setFillStyle("#FFFFFF")
+      c.setTextAlign('left')
+      c.fillText(text, x + 3.5 * rem, y + 2 * rem, 8 * rem)
+    }
+
+    for (let i = 0; i < barrageStyle.length; i++) {
+      _drawItem(barrageStyle[i].left, barrageStyle[i].top, barrageStyle[i].avatar, barrageStyle[i].text)
+    }
+
+    c.draw()
+  },
+
 })
